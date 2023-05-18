70871e74488d19d0ae0cca102018884eac34b396
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-18 10:24:12 +0300

    solver

--- app/services/sudoku_solver.rb
+++ app/services/sudoku_solver.rb
@@ -7,12 +7,11 @@ class SudokuSolver
     row, col = find
 
     (1..9).each do |num|
-      if valid?(@grid, row, col, num)
+      if valid?(row, col, num)
         @grid[row][col] = num
 
-        if solve(@grid)
-          return @grid
-        end
+        return @grid if solve(@grid)
+
         @grid[row][col] = 0
       end
     end
@@ -23,34 +22,25 @@ class SudokuSolver
   def self.find_empty_cell(grid)
     grid.each_with_index do |row, i|
       row.each_with_index do |cell, j|
-        if cell == 0
-          return [i, j]
-        end
+        return [i, j] if cell.zero?
       end
     end
+
     nil
   end
 
-  def self.valid?(grid, row, col, num)
+  def self.valid?(row, col, num)
     (0..8).each do |i|
-      if grid[row][i] == num
-        return false
-      end
-    end
-
-    (0..8).each do |i|
-      if grid[i][col] == num
-        return false
-      end
+      return false if @grid[row][i] == num
+      return false if @grid[i][col] == num
     end
 
     box_row = (row / 3) * 3
     box_col = (col / 3) * 3
+
     (0..2).each do |i|
       (0..2).each do |j|
-        if grid[box_row + i][box_col + j] == num
-          return false
-        end
+        return false if @grid[box_row + i][box_col + j] == num
       end
     end
 
