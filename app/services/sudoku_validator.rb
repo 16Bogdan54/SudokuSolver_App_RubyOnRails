a45c5d1f3fe2abe05513040e6f95f807dbde2c7a
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-18 10:26:08 +0300

    validator

--- app/services/sudoku_validator.rb
+++ app/services/sudoku_validator.rb
@@ -1,30 +1,24 @@
 class SudokuValidator
   def self.solved?(grid)
-    (0..8).each { |row|
-      unless valid_group?(grid[row])
-        return false
-      end
-    }
+    (0..8).each do |row|
+      return false unless valid_group?(grid[row])
+    end
 
-    (0..8).each { |col|
-      unless valid_group?(grid.map { |row| row[col] })
-        return false
-      end
-    }
+    (0..8).each do |col|
+      return false unless valid_group?(grid.map { |row| row[col] })
+    end
 
-    (0..2).each { |box_row|
-      (0..2).each { |box_col|
+    (0..2).each do |box_row|
+      (0..2).each do |box_col|
         box = []
-        (0..2).each { |row|
-          (0..2).each { |col|
+        (0..2).each do |row|
+          (0..2).each do |col|
             box << grid[box_row * 3 + row][box_col * 3 + col]
-          }
-        }
-        unless valid_group?(box)
-          return false
+          end
         end
-      }
-    }
+        return false unless valid_group?(box)
+      end
+    end
 
     true
   end
